547cbdb7dc4f3778ed3b4148d609c23c636ca705
juandepablodev <juandepablo.dev@gmail.com>
2026-08-03 21:56:07 +0200

    fix: make SwipeDecision conform to Codable for session persistence

--- codigo/TinderCleaner/Features/SwipeEngine/Models/SwipeDecision.swift
+++ codigo/TinderCleaner/Features/SwipeEngine/Models/SwipeDecision.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// Represents a classification decision made on a photo or video card.
-public enum SwipeDecision: Sendable, Equatable {
+public enum SwipeDecision: String, Codable, Sendable, Equatable {
   case keep
   case delete
 }
